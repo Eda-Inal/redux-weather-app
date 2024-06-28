7b6a2a95ec51015310ffb9620974f828cf3e0db0
Eda Işıl İnal <edainalisil@gmail.com>
2024-06-28 13:50:25 +0300

    created react,redux and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useDispatch, useSelector } from "react-redux";
+
+function App() {
+  const dispatch = useDispatch();
+  
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold text-slate-400">
+     Hello
+    </h1>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/redux/weatherSlice.jsx
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const weatherSlice = createSlice({
+    name : 'weather',
+    initialState : {
+       
+    },
+    reducers : {
+
+    }
+
+})
+
+// export const {reducer function} = weatherSlice.actions
+export default weatherSlice.reducer;
