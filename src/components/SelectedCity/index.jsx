e43ffbeab476676ac6ec80efdb6133b3dc8d1403
Eda Işıl İnal <edainalisil@gmail.com>
2024-06-29 12:42:03 +0300

    update updateCity

--- src/components/SelectedCity/index.jsx
+++ src/components/SelectedCity/index.jsx
@@ -1,11 +1,12 @@
 import cities from "../../cities/cities.json"
 import { useSelector,useDispatch } from "react-redux";
-import { updateCity } from "../../redux/weatherSlice";
+import { updateCity} from "../../redux/weatherSlice";
 
 function SelectedCity() {
   const dispatch = useDispatch();
-  const cityname = useSelector((state) => state.weather.cityname);
+  const {cityname,id} = useSelector((state) => state.weather);
   console.log(cityname);
+  console.log(id);
 
   const handleUpdateCity = (cityname) => {
     dispatch(updateCity(cityname));
@@ -15,15 +16,15 @@ function SelectedCity() {
     return (
       <div>
    <select className="m-6" onChange={(event) => handleUpdateCity(event.target.value)}>
-    <option value="">Choose a city   </option>
+    <option value="">{cityname}  </option>
         {cities.map((city) =>(
- <option key={city.id} value={city.name}   >
+ <option key={city.id} value={city.name}    >
  {city.name}
 </option>
         ))}
   
    </select>
-   <div className="text-5xl text-red-500">{cityname}</div>
+   <div className="text-5xl text-red-500">{cityname} {id}</div>
       </div>
     )
   }
--- src/redux/weatherSlice.jsx
+++ src/redux/weatherSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice} from '@reduxjs/toolkit';
+import cities from "../cities/cities.json"
 
 
 
@@ -7,13 +8,24 @@ import { createSlice} from '@reduxjs/toolkit';
 const weatherSlice = createSlice({
   name: 'weather',
   initialState: {
+    id : 35,
    cityname:"İzmir",
+   latitude : "38.4189",
+   longitude :"27.1287"
+
 
   },
   reducers: {
   updateCity : (state,action) => {
+
 state.cityname = action.payload
-  }
+let data = cities.find(city => city.name === state.cityname) ;
+state.id = data.id;
+state.latitude = data.latitude;
+state.longitude =data.longitude
+console.log(data.name,data.latitude);
+  },
+
   },
 
 });
