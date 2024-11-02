5a83dfcc3777688f990ca0e7b9c37e092b31845b
Eda Işıl İnal <edainalisil@gmail.com>
2024-11-02 16:04:49 +0300

    something fixed

--- src/components/SelectedCity/index.jsx
+++ src/components/SelectedCity/index.jsx
@@ -1,33 +1,33 @@
 import cities from "../../cities/cities.json"
-import { useSelector,useDispatch } from "react-redux";
-import { updateCity} from "../../redux/weatherSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { updateCity } from "../../redux/weatherSlice";
 
 function SelectedCity() {
   const dispatch = useDispatch();
-  const {cityname} = useSelector((state) => state.weather);
+  const { cityname } = useSelector((state) => state.weather);
 
- 
+
 
   const handleUpdateCity = (cityname) => {
     dispatch(updateCity(cityname));
   }
-    
 
-    return (
-      <div>
-   <select className="m-6 mb-12 w-2/4 lg:w-2/5 p-2 rounded-2xl  bg-blue-200 font-medium  text-blue-950  pl-3 " onChange={(event) => handleUpdateCity(event.target.value)}>
-    <option value="">{cityname}  </option>
-        {cities.map((city) =>(
- <option key={city.id} value={city.name}    >
- {city.name}
-</option>
+
+  return (
+    <div>
+      <select className="m-6 mb-12 w-2/4 lg:w-2/5 p-2 rounded-2xl  bg-blue-200 font-medium  text-blue-950  pl-3 " onChange={(event) => handleUpdateCity(event.target.value)}>
+        <option value="">{cityname}  </option>
+        {cities.map((city) => (
+          <option key={city.id} value={city.name}    >
+            {city.name}
+          </option>
         ))}
-  
-   </select>
-   {/* <div className="text-5xl text-red-500">{cityname}</div> */}
- 
-      </div>
-    )
-  }
-  
-  export default SelectedCity
+
+      </select>
+      {/* <div className="text-5xl text-red-500">{cityname}</div> */}
+
+    </div>
+  )
+}
+
+export default SelectedCity
--- src/redux/weatherSlice.jsx
+++ src/redux/weatherSlice.jsx
@@ -1,13 +1,8 @@
-import { createSlice,createAsyncThunk} from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import cities from "../cities/cities.json"
 
 
-
-
 const API_KEY = process.env.REACT_APP_API_KEY;
-console.log('API Key:', API_KEY);
-
-
 
 export const fetchWeatherData = createAsyncThunk(
   'weather/fetchWeatherData',
@@ -23,26 +18,26 @@ export const fetchWeatherData = createAsyncThunk(
 const weatherSlice = createSlice({
   name: 'weather',
   initialState: {
-    id : 35,
-   cityname:"İzmir",
-   latitude : "38.4189",
-   longitude :"27.1287",
-   weatherData: null,
-   status: 'idle',
-   error: null,
+    id: 35,
+    cityname: "İzmir",
+    latitude: "38.4189",
+    longitude: "27.1287",
+    weatherData: null,
+    status: 'idle',
+    error: null,
 
 
   },
   reducers: {
-  updateCity : (state,action) => {
+    updateCity: (state, action) => {
 
-state.cityname = action.payload
-let data = cities.find(city => city.name === state.cityname) ;
-state.id = data.id;
-state.latitude = data.latitude;
-state.longitude =data.longitude
-console.log(data.name,data.latitude);
-  },
+      state.cityname = action.payload
+      let data = cities.find(city => city.name === state.cityname);
+      state.id = data.id;
+      state.latitude = data.latitude;
+      state.longitude = data.longitude
+      console.log(data.name, data.latitude);
+    },
 
   },
   extraReducers: (builder) => {
@@ -62,5 +57,5 @@ console.log(data.name,data.latitude);
 
 });
 
- export const { updateCity} = weatherSlice.actions;
+export const { updateCity } = weatherSlice.actions;
 export default weatherSlice.reducer;
